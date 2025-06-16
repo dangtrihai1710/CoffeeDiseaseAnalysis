75dd8693b5d0f9e363aafea4f4ea78e6dfc09569
admin <haic1947@gmail.com>
2025-06-17 00:14:53 +0700

    Add project files.

---
+++ CoffeeDiseaseAnalysis/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoffeeDiseaseAnalysis.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets sẽ được thêm sau khi tạo các Entity classes
+        // public DbSet<LeafImage> LeafImages { get; set; }
+        // public DbSet<Prediction> Predictions { get; set; }
+        // public DbSet<Feedback> Feedbacks { get; set; }
+        // ... các DbSet khác
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configurations sẽ được thêm sau
+        }
+    }
+}
---
+++ CoffeeDiseaseAnalysis/Program.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection")
+    ?? "Server=(localdb)\\mssqllocaldb;Database=CoffeeDiseaseDb;Trusted_Connection=true;MultipleActiveResultSets=true;TrustServerCertificate=true";
+
+builder.Services.AddDbContext<CoffeeDiseaseAnalysis.Data.ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+// Cấu hình Identity
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+})
+    .AddRoles<IdentityRole>()
+    .AddEntityFrameworkStores<CoffeeDiseaseAnalysis.Data.ApplicationDbContext>();
+
+// Add Redis Cache - tạm thời comment vì có thể chưa cài Redis
+// builder.Services.AddStackExchangeRedisCache(options =>
+// {
+//     options.Configuration = builder.Configuration.GetConnectionString("Redis") ?? "localhost:6379";
+// });
+
+// Add Controllers
+builder.Services.AddControllers();
+
+// Add Swagger (OpenAPI đã có sẵn)
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new()
+    {
+        Title = "Coffee Disease Analysis API",
+        Version = "v1",
+        Description = "API cho phân tích bệnh lá cây cà phê"
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Coffee Disease Analysis API v1");
+        c.RoutePrefix = "swagger";
+    });
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
